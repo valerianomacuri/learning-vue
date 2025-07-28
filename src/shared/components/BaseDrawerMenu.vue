644b522c47862fd6dfdd41f5698eb7c1a61566c0
Leonardo Valeriano <leonardo.valeriano.public@gmail.com>
2025-07-28 18:00:32 -0500

    update menu

--- src/shared/components/BaseDrawerMenu.vue
+++ src/shared/components/BaseDrawerMenu.vue
@@ -2,13 +2,19 @@
   <ul class="menu text-base-content min-h-full w-80 p-4 bg-base-100">
     <template v-for="item in menu" :key="item.path">
       <li>
-        <!-- <h2 class="menu-title">Estado</h2> -->
-        <a>{{ item.name }}</a>
-        <ul v-if="item.children">
-          <li v-for="child in item.children" :key="child.path">
-            <a>{{ child.name }}</a>
-          </li>
-        </ul>
+        <details open>
+          <summary>{{ item.name }}</summary>
+          <ul v-if="item.children">
+            <li v-for="subItem in item.children" :key="subItem.path">
+              <a class="menu-title">{{ subItem.name }}</a>
+              <ul v-if="subItem.children">
+                <li v-for="lastItem in subItem.children" :key="lastItem.path">
+                  <a>{{ lastItem.name }}</a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </details>
       </li>
     </template>
   </ul>
--- src/shared/data/menu.ts
+++ src/shared/data/menu.ts
@@ -1,23 +1,238 @@
 interface Menu {
-  name: string
-  path?: string
   children?: Menu[]
+  name?: string
+  path?: string
+  title?: string
 }
 
 export const menu: Menu[] = [
   {
-    name: 'Estado',
-    path: 'state',
+    name: 'Fudamentos',
+    path: 'fundamentals',
     children: [
       {
-        name: 'ref',
-        path: '#ref',
+        name: 'Instalación',
+        children: [
+          {
+            name: 'create-vue',
+          },
+        ],
       },
       {
-        name: 'reactive',
-        path: '#reactive',
+        name: 'Sintaxis',
+        path: 'syntax',
+        children: [
+          {
+            name: 'Vue templates',
+            path: '#vue-templates',
+          },
+        ],
+      },
+      {
+        name: 'Estilos',
+        path: '',
+        children: [
+          {
+            name: '<style scoped>',
+            path: '#style-scoped',
+          },
+        ],
       },
     ],
   },
-  { name: '' },
+  {
+    name: 'Declaración de componentes',
+    path: 'declaration',
+    children: [
+      {
+        name: 'Moderna',
+        path: 'modern',
+        children: [
+          {
+            name: '<script setup>',
+            path: '#script-setup',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: 'Manejo del estado local',
+    path: 'local-state',
+    children: [
+      {
+        name: 'Estado',
+        children: [
+          {
+            name: 'ref',
+            path: '#ref',
+          },
+          {
+            name: 'reactive',
+            path: '#reactive',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: 'Visualización de datos',
+    path: 'data-display',
+    children: [
+      {
+        name: 'Condicionales',
+        children: [
+          {
+            name: 'v-if',
+            path: '#v-if',
+          },
+          {
+            name: 'v-else-if',
+            path: '#v-else-if',
+          },
+          {
+            name: 'v-show',
+            path: '#v-show',
+          },
+        ],
+      },
+      {
+        name: 'Listas',
+        children: [
+          {
+            name: 'v-for',
+            path: '#v-for',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: 'Comunicación entre componentes',
+    path: 'communication',
+    children: [
+      {
+        name: 'Padre → hijo',
+        children: [
+          {
+            name: 'defineProps()',
+            path: '#define-props',
+          },
+        ],
+      },
+      {
+        name: 'Hijo → padre',
+        children: [
+          {
+            name: 'emit($event)',
+            path: '#emit',
+          },
+        ],
+      },
+      {
+        name: 'Entre hermanos',
+        children: [
+          {
+            name: 'provide/inject',
+            path: '#provide-inject',
+          },
+        ],
+      },
+      {
+        name: 'Slots / Children',
+        path: 'slots-children',
+        children: [
+          {
+            name: '<slot>',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: 'Ciclo de vida del componente',
+    path: 'lifecycle',
+    children: [
+      {
+        name: 'Montaje',
+        path: 'mounting',
+        children: [
+          {
+            name: 'onBeforeMount()',
+            path: 'on-before-mount',
+          },
+          {
+            name: 'onMount()',
+            path: 'on-mount',
+          },
+        ],
+      },
+      {
+        name: 'Actualización',
+      },
+      {
+        name: 'Desmontaje',
+      },
+    ],
+  },
+  {
+    name: 'Enrutamiento',
+    path: 'routing',
+    children: [
+      {
+        name: 'Configuración',
+        path: 'configuration',
+      },
+      {
+        name: 'Rutas anidadas',
+        path: 'nested-routes',
+      },
+      {
+        name: 'Navegación',
+        path: 'navigation',
+      },
+      {
+        name: 'Navegación programática',
+        path: 'programmatic-navigation',
+      },
+    ],
+  },
+  {
+    name: 'Manejo del estado global',
+    path: 'global-state',
+    children: [
+      {
+        name: 'Pinia',
+        path: 'pinia',
+      },
+    ],
+  },
+  {
+    name: 'Performance y Optimización',
+    path: 'performance',
+    children: [
+      {
+        name: 'Lazy Loading',
+        children: [
+          {
+            name: 'defineAsyncComponent()',
+          },
+        ],
+      },
+      {
+        name: 'Memoización',
+        children: [
+          {
+            name: 'computed()',
+          },
+          {
+            name: 'watch()',
+          },
+          {
+            name: 'cache()',
+          },
+        ],
+      },
+    ],
+  },
 ]
